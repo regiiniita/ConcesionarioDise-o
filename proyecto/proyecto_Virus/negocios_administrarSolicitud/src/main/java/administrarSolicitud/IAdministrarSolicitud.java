751353c6e5e21d6b41a981714dbf9d401154801e
ernestoCis <jesus.cisneros262722@potros.itson.edu.mx>
2026-04-14 17:09:21 -0700

    administrarSolicitud

--- proyecto/proyecto_Virus/negocios_administrarSolicitud/src/main/java/administrarSolicitud/IAdministrarSolicitud.java
+++ proyecto/proyecto_Virus/negocios_administrarSolicitud/src/main/java/administrarSolicitud/IAdministrarSolicitud.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package interfaces;
+package administrarSolicitud;
 
 /**
  *
- * @author jesus
+ * @author 
  */
 public interface IAdministrarSolicitud {
     
