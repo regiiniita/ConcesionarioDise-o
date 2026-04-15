a36dbb143d5ad907ce6f415fe7d1581af070a409
ernestoCis <jesus.cisneros262722@potros.itson.edu.mx>
2026-04-14 18:06:46 -0700

    subsistema administrarSolicitud

---
+++ proyecto/proyecto_Virus/negocios_administrarSolicitud/src/main/java/administrarSolicitud/FachadaAdministrarSolicitud.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package administrarSolicitud;
+
+import dto.ClienteDTO;
+import dto.SolicitudDTO;
+import enums.EstadoSolicitudDTO;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author
+ */
+public class FachadaAdministrarSolicitud implements IAdministrarSolicitud{
+
+    @Override
+    public SolicitudDTO iniciarNuevaSolicitud(ClienteDTO cliente) {
+        SolicitudDTO nueva = new SolicitudDTO();
+        nueva.setCliente(cliente);
+        nueva.setEstado(EstadoSolicitudDTO.BORRADOR);
+        return nueva;
+    }
+
+    @Override
+    public SolicitudDTO agregarDatosFinancieros(SolicitudDTO solicitud, String tipoEmpleo, String puesto, String tipoContrato, String nombreEmpresa, String antiguedadLaboral, Double ingresoMensualBruto, Double ingresoMensualNeto, Double gastosMensuales, String rutaComprobanteIngresos, String rutaComprobanteEmpleo) {
+        solicitud.setTipoEmpleo(tipoEmpleo);
+        solicitud.setPuesto(puesto);
+        solicitud.setTipoContrato(tipoContrato);
+        solicitud.setNombreEmpresa(nombreEmpresa);
+        solicitud.setAntiguedadLaboral(antiguedadLaboral);
+        solicitud.setIngresoMensualBruto(ingresoMensualBruto);
+        solicitud.setIngresoMensualNeto(ingresoMensualNeto);
+        solicitud.setGastosMensuales(gastosMensuales);
+        solicitud.setRutaComprobanteIngresos(rutaComprobanteIngresos);
+        solicitud.setRutaComprobanteEmpleo(rutaComprobanteEmpleo);
+        
+        return solicitud;
+    }
+
+    @Override
+    public SolicitudDTO confirmarEnvioSolicitud(SolicitudDTO solicitud) {
+        solicitud.setEstado(EstadoSolicitudDTO.ENVIADA);
+        
+        //persistir solicitud
+        
+        JOptionPane.showMessageDialog(null, "Solicitud enviada");
+        return solicitud;
+    }
+
+    @Override
+    public List<SolicitudDTO> obtenerSolicitudesPorCliente(String idCliente) {
+        return null;
+    }
+}
