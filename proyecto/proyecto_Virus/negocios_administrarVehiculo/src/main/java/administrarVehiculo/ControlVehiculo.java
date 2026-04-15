6093fbc47c92e7e526b965c88f77341a80d7096b
regiiniita <reginamenesesj09@gmail.com>
2026-04-14 17:40:17 -0700

    fix: ControlVehiculo

--- proyecto/proyecto_Virus/negocios_administrarVehiculo/src/main/java/administrarVehiculo/ControlVehiculo.java
+++ proyecto/proyecto_Virus/negocios_administrarVehiculo/src/main/java/administrarVehiculo/ControlVehiculo.java
@@ -5,27 +5,94 @@
 package administrarVehiculo;
 
 import dto.VehiculoDTO;
+import enums.EstadoVehiculoDTO;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Control interno que simula el catálogo de vehículos (Mock).
+ * Control interno que simula el catálogo de vehículos con el nuevo constructor.
+ * @author regina
  */
 class ControlVehiculo {
 
     protected ControlVehiculo() {}
 
-    /**
-     * Devuelve una lista simulada que incluye el vehículo del escenario.
-     */
     protected List<VehiculoDTO> obtenerCatalogoDisponibles() {
-        System.out.println("Cargando catálogo de vehículos...");
         List<VehiculoDTO> catalogo = new ArrayList<>();
         
-        // Datos del escenario: Honda CR-V EX-L 2024 ($589,000)
-        catalogo.add(new VehiculoDTO(101, "Honda", "CR-V EX-L 2024", 589000.0));
+        catalogo.add(new VehiculoDTO(
+            "HON7890123456",        
+            "Honda",                
+            "CR-V EX-L",            
+            2024,                  
+            589000.0,              
+            EstadoVehiculoDTO.DISPONIBLE             
+        ));
 
+        catalogo.add(new VehiculoDTO(
+            "TOY1234567890", 
+            "Toyota", 
+            "RAV4", 
+            2024, 
+            610000.0, 
+            EstadoVehiculoDTO.DISPONIBLE     
+        ));
+
+        catalogo.add(new VehiculoDTO(
+            "MAZ4567890123", 
+            "Mazda", 
+            "CX-5", 
+            2023, 
+            550000.0, 
+            EstadoVehiculoDTO.DISPONIBLE     
+        ));
         
+        catalogo.add(new VehiculoDTO(
+            "NISS9876543210", 
+            "Nissan", 
+            "Versa Advance", 
+            2023, 
+            340000.0, 
+            EstadoVehiculoDTO.DISPONIBLE  
+        ));
+
+        catalogo.add(new VehiculoDTO(
+            "BMW1122334455", 
+            "BMW", 
+            "X3 xDrive30i", 
+            2024, 
+            1250000.0, 
+            EstadoVehiculoDTO.DISPONIBLE  
+        ));
+
+        catalogo.add(new VehiculoDTO(
+            "FORD5566778899", 
+            "Ford", 
+            "Ranger XLT", 
+            2024, 
+            785000.0, 
+            EstadoVehiculoDTO.DISPONIBLE  
+        ));
+
+        catalogo.add(new VehiculoDTO(
+            "SEAT3344556677", 
+            "SEAT", 
+            "Ibiza FR", 
+            2023, 
+            415000.0, 
+            EstadoVehiculoDTO.DISPONIBLE  
+        ));
+
+        catalogo.add(new VehiculoDTO(
+            "TESL9988776655", 
+            "Tesla", 
+            "Model 3", 
+            2024, 
+            899000.0, 
+            EstadoVehiculoDTO.DISPONIBLE  
+        ));
+
+        System.out.println("Catálogo cargado con " + catalogo.size() + " vehículos.");
         return catalogo;
     }
 }
