010b4e2dc39d6eeaf84f6460511a1f01ac738aff
regiiniita <reginamenesesj09@gmail.com>
2026-04-14 17:02:51 -0700

    feat: Creacion de proyecto de subsistema administarCliente y de dto.negocios

---
+++ proyecto/proyecto_Virus/negocios_administrarCliente/src/main/java/administrarCliente/Negocios_administrarCliente.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package administrarCliente;
+
+/**
+ *
+ * @author regin
+ */
+public class Negocios_administrarCliente {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
