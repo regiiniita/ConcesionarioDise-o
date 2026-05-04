41e7fd52e02432a5ffda4bbadc9a84c94ac6abc2
mariianass <omariana1073@gmail.com>
2026-05-04 09:28:17 -0700

    feat: creación clases

---
+++ proyecto/proyecto_Virus/negocios_administrarCliente/src/main/java/administrarCliente/ValidadorCliente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package administrarCliente;
+
+/**
+ *
+ * @author regina, mariana, ernesto, isaac y luis
+ */
+public class ValidadorCliente {
+    
+    public void validarNombre(){
+        
+    }
+    
+}
